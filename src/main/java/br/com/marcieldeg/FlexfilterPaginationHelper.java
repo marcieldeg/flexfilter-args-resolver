754cd5a8f9710ab24b7cd24f764c361754c0ebbc
marcieldeg <marciel.deg@gmail.com>
2023-03-22 06:51:22 -0300

    Refactoring class

--- src/main/java/br/com/marcieldeg/FlexfilterPaginationHelper.java
+++ src/main/java/br/com/marcieldeg/FlexfilterPaginationHelper.java
@@ -11,22 +11,29 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
 
 public class FlexfilterPaginationHelper {
+	private static final int DEFAULT_PAGE = 1;
+	private static final int DEFAULT_SIZE = 50;
+
 	public static Pageable fromFilters(Map<String, String[]> filters) {
-		final int page = filters.containsKey("page") ? Integer.parseInt(filters.remove("page")[0]) : 0;
-		final int size = filters.containsKey("size") ? Integer.parseInt(filters.remove("size")[0]) : 0;
+		final int page = filters.containsKey("page") ? Integer.parseInt(filters.remove("page")[0]) : DEFAULT_PAGE;
+		final int size = filters.containsKey("size") ? Integer.parseInt(filters.remove("size")[0]) : DEFAULT_SIZE;
+
+		if (page < 1)
+			throw new IllegalStateException("Invalid default page size. Must not be less than one");
+
+		if (!filters.containsKey("sort"))
+			return PageRequest.of(page, size);
 
 		final List<Order> orderList = new ArrayList<>();
-		if (filters.containsKey("sort")) {
-			for (String sortValue : filters.remove("sort")) {
-				if (sortValue.contains(",")) {
-					String[] sortParts = sortValue.split(",");
-					orderList.add(new Order(Direction.valueOf(sortParts[1].toUpperCase()), sortParts[0]));
-				} else {
-					orderList.add(Order.asc(sortValue));
-				}
+		for (String sortValue : filters.remove("sort")) {
+			if (sortValue.contains(",")) {
+				String[] sortParts = sortValue.split(",");
+				orderList.add(new Order(Direction.valueOf(sortParts[1].toUpperCase()), sortParts[0]));
+			} else {
+				orderList.add(Order.asc(sortValue));
 			}
 		}
 
-		return orderList.isEmpty() ? PageRequest.of(page, size) : PageRequest.of(page, size, Sort.by(orderList));
+		return PageRequest.of(page, size, Sort.by(orderList));
 	}
 }
